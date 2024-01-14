a5b55f4b2430c2b33aaab2d1e3244c4013de2c8c
Yuntai Song <songyuntai704@gmail.com>
2024-01-14 12:31:26 -0800

    Update slaterjastrow.py

--- netket/models/slaterjastrow.py
+++ netket/models/slaterjastrow.py
@@ -2,60 +2,77 @@ import flax.linen as nn
 import jax.numpy as jnp
 from jax.nn.initializers import normal
 from functools import partial
+import numpy as np
 
 from netket.utils.types import DType, Array, NNInitFunc
 from netket.experimental.hilbert import SpinOrbitalFermions
-from netket.nn.masked_linear import default_kernel_init
 from netket import jax as nkjax
 
 class slater_jastrow(nn.Module):
     hilbert: SpinOrbitalFermions
-    kernel_init: NNInitFunc = normal()
+    jastrow_kernel_init: NNInitFunc = normal()
+    slater_kernel_init: NNInitFunc = normal()
     param_dtype: DType = jnp.complex128
     restricted: bool = True  # Assuming a restricted setting for simplicity
 
     def setup(self):
-        nv = self.hilbert.size
-        self.kernel = self.param("kernel", self.kernel_init, (nv, nv), self.param_dtype)
-        self.kernel = self.kernel + self.kernel.T
+        # Slater part setup
+        if self.restricted:
+            self.M = self.param(
+                "M",
+                self.slater_kernel_init,
+                (self.hilbert.n_orbitals, self.hilbert.n_fermions_per_spin[0]),
+                self.param_dtype,
+            )
+            self.orbitals = [self.M for _ in self.hilbert.n_fermions_per_spin]
+        else:
+            self.orbitals = [
+                self.param(
+                    f"M_{i}",
+                    self.slater_kernel_init,
+                    (self.hilbert.n_orbitals, nf_i),
+                    self.param_dtype,
+                )
+                for i, nf_i in enumerate(self.hilbert.n_fermions_per_spin)
+            ]
 
-        # Setting up the orbital parameters (assuming restricted setting)
-        self.M = self.param(
-            "M",
-            self.kernel_init,
-            (self.hilbert.n_orbitals, self.hilbert.n_fermions_per_spin[0]),
-            self.param_dtype,
+        # Jastrow part setup
+        nv = self.hilbert.size
+        self.jastrow_kernel = self.param(
+            "jastrow_kernel", 
+            self.jastrow_kernel_init, 
+            (nv * (nv - 1) // 2,), 
+            self.param_dtype
         )
-        self.orbitals = [self.M for _ in self.hilbert.n_fermions_per_spin]
 
     def log_jastrow(self, x_in: Array):
-        # Determine the highest precision data type between kernel and x_in
-        dtype = jnp.result_type(self.kernel, x_in)
-    
-        # Convert both kernel and x_in to this highest precision data type
-        kernel = self.kernel.astype(dtype)
-        x_in = x_in.astype(dtype)
+        nv = x_in.shape[-1]
+        il = jnp.tril_indices(nv, k=-1)
 
-        y = jnp.einsum("...i,ij,...j", x_in, kernel, x_in)
+        # Retrieve and reshape Jastrow kernel
+        jastrow_kernel = self.jastrow_kernel
+        W = jnp.zeros((nv, nv), dtype=self.param_dtype).at[il].set(jastrow_kernel)
+
+        # Compute Jastrow term
+        W, x_in = jnp.promote_types(W, x_in)
+        y = jnp.einsum("...i,ij,...j", x_in, W, x_in)
+
         return y
 
-    def log_slater_determinant(self, n):
-        @partial(jnp.vectorize, signature="(n)->()")
-        def log_sd(n):
-            R = n.nonzero(size=self.hilbert.n_fermions)[0]
-            log_det_sum = 0
-            i_start = 0
-            for i, (n_fermions_i, M_i) in enumerate(
-                zip(self.hilbert.n_fermions_per_spin, self.orbitals)
-            ):
-                R_i = R[i_start : i_start + n_fermions_i] - i * self.hilbert.n_orbitals
-                A_i = M_i[R_i]
-                log_det_sum = log_det_sum + nkjax.logdet_cmplx(A_i)
-                i_start += n_fermions_i
+    def log_sd(self, n):
+        # Compute Slater determinant
+        R = n.nonzero(size=self.hilbert.n_fermions)[0]
+        log_det_sum = 0
+        i_start = 0
+        for i, (n_fermions_i, M_i) in enumerate(
+            zip(self.hilbert.n_fermions_per_spin, self.orbitals)
+        ):
+            R_i = R[i_start : i_start + n_fermions_i] - i * self.hilbert.n_orbitals
+            A_i = M_i[R_i]
+            log_det_sum = log_det_sum + nkjax.logdet_cmplx(A_i)
+            i_start = n_fermions_i
 
-            return log_det_sum
-
-        return log_sd(n)
+        return log_det_sum
 
     def __call__(self, n):
         if not n.shape[-1] == self.hilbert.size:
@@ -64,10 +81,7 @@ class slater_jastrow(nn.Module):
                 f"degrees of freedom, but got a sample of shape {n.shape}."
             )
 
-        # Compute Jastrow term
-        y = self.log_jastrow(n)
+        log_sd_val = self.log_sd(n)  # Compute log-Slater determinant
+        y = self.log_jastrow(n)      # Compute Jastrow term
 
-        # Compute log-slater determinant
-        log_sd = self.log_slater_determinant(n)
-
-        return log_sd + y
+        return log_sd_val + y
